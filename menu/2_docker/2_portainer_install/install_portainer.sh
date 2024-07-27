9574d0e0c8df044a97ff566264f7d7aca015c518
szilardshomelab <farkas.szilard@icloud.com>
2024-07-27 15:54:30 +0300

    Update install_portainer.sh

--- menu/2_docker/2_portainer_install/install_portainer.sh
+++ menu/2_docker/2_portainer_install/install_portainer.sh
@@ -2,7 +2,7 @@
 
 # Ask the user to confirm the installation
 while true; do
-    read -p "Are you sure you want to install prowlarr? (y/n): " CONFIRM
+    read -p "Are you sure you want to install portainer? (y/n): " CONFIRM
     case "$CONFIRM" in
         [yY]* ) break;;   # If 'y' or 'Y', proceed
         [nN]* ) echo "Installation aborted."; exit 1;;  # If 'n' or 'N', exit
@@ -12,10 +12,10 @@ done
 
 # Define the paths
 ENV_FILE="/opt/appdata/.network.env"
-TEMPLATE_FILE="/opt/szilardshomelab/appdata/prowlarr/compose-template.yml"
-mkdir -p /opt/appdata/prowlarr
-touch /opt/appdata/prowlarr/compose.yml
-OUTPUT_FILE="/opt/appdata/prowlarr/compose.yml"
+TEMPLATE_FILE="/opt/szilardshomelab/appdata/portainer/compose-template.yml"
+mkdir -p /opt/appdata/portainer
+touch /opt/appdata/portainer/compose.yml
+OUTPUT_FILE="/opt/appdata/portainer/compose.yml"
 
 # Load environment variables from the .env file
 export $(grep -v '^#' $ENV_FILE | xargs)
@@ -31,10 +31,10 @@ echo "Docker Compose services started!"
 # Get the server's IP address
 SERVER_IP=$(hostname -I | awk '{print $1}')
 
-# Assuming prowlarr is exposed on port 9000
-prowlarr_PORT=9443
+# Assuming portainer is exposed on port 9000
+portainer_PORT=9443
 
 # Construct the access URL
-ACCESS_URL="https://$SERVER_IP:$prowlarr_PORT"
+ACCESS_URL="https://$SERVER_IP:$portainer_PORT"
 
-echo "Access prowlarr at: $ACCESS_URL"
+echo "Access portainer at: $ACCESS_URL"
