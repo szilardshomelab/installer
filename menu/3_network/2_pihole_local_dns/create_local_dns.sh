19597fa3612e34c980c5d98ea6c04b1208b54072
szilardshomelab <farkas.szilard@icloud.com>
2024-07-31 19:20:46 +0300

    Update create_local_dns.sh

--- menu/3_network/2_pihole_local_dns/create_local_dns.sh
+++ menu/3_network/2_pihole_local_dns/create_local_dns.sh
@@ -8,17 +8,17 @@ ENV_FILE_PATH="/opt/appdata/pihole/.api.env"
 # Create the environment file directory if it doesn't exist
 mkdir -p "$(dirname "$ENV_FILE_PATH")"
 
-# Extract the API key from the Pi-hole container
-API_KEY=$(sudo docker exec "$CONTAINER_NAME" awk -F'=' '/^API_KEY/ {print $2}' "$CONTAINER_PATH")
+# Extract the WEBPASSWORD from the Pi-hole container
+WEBPASSWORD=$(sudo docker exec "$CONTAINER_NAME" awk -F'=' '/^WEBPASSWORD/ {print $2}' "$CONTAINER_PATH")
 
-# Check if API_KEY was found and not empty
-if [ -z "$API_KEY" ]; then
-  echo "Error: API_KEY not found in $CONTAINER_PATH"
+# Check if WEBPASSWORD was found and not empty
+if [ -z "$WEBPASSWORD" ]; then
+  echo "Error: WEBPASSWORD not found in $CONTAINER_PATH"
   exit 1
 fi
 
-# Write the API key to the environment file
-echo "PIHOLE_API_KEY=$API_KEY" > "$ENV_FILE_PATH"
+# Write the WEBPASSWORD to the environment file
+echo "PIHOLE_API_KEY=$WEBPASSWORD" > "$ENV_FILE_PATH"
 
 # Provide feedback
 echo "API key written to $ENV_FILE_PATH"
